de7b748efccb1b7d76d6d85c637aca47cee88b12
UINya <l1792669710@163.com>
2026-09-09 20:07:20 +0900

    新增鼠鼠战斗

--- Scripts/Encounter/MaxiWeakEncounter.cs
+++ Scripts/Encounter/MaxiWeakEncounter.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Encounters;
 using MegaCrit.Sts2.Core.Models.Monsters;
+using MegaCrit.Sts2.Core.Random;
 using MegaCrit.Sts2.Core.Rooms;
 using STS2RitsuLib.Scaffolding.Content;
 using MaxiMonster = RabbitAndSteelNewMap.Scripts.Monster.Maxi;
@@ -10,6 +12,19 @@ namespace RabbitAndSteelNewMap.Scripts.Encounter;
 
 public sealed class MaxiWeak : ModEncounterTemplate
 {
+    protected override bool UseActCombatBackground => false;
+
+    protected override bool UseProgrammaticCombatBackground => true;
+
+    protected override BackgroundAssets? BuildProgrammaticCombatBackground(ActModel parentAct, Rng rng)
+    {
+        _ = parentAct;
+        _ = rng;
+        return CombatBackgroundAssetsFactory.Create(
+            "res://mod/Sence/KingdomOutskirts.tscn",
+            Array.Empty<string>());
+    }
+
     public override RoomType RoomType => RoomType.Monster;
 
     public override bool IsWeak => true;
